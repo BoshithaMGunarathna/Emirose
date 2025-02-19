dfacf93bdbccdb3d6aa24d848d89182893976758
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-19 11:53:17 +0530

    Create serviceHasSubRoutes.js

---
+++ backend/routes/serviceHasSubRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const serviceHasThingsController = require('../controllers/serviceHasSubController');
+
+const router = express.Router();
+
+router.post("/", serviceHasThingsController.createServiceHasThing);
+router.get("/:serviceId", serviceHasThingsController.getAllServiceHasThings);
+router.get("/thing/:id", serviceHasThingsController.getServiceHasThingById);
+router.put("/thing/:id", serviceHasThingsController.updateServiceHasThing);
+router.put("/:serviceId", serviceHasThingsController.updateBulkServiceHasThings);
+router.delete("/thing/:id", serviceHasThingsController.deleteServiceHasThing);
+
+module.exports = router;
