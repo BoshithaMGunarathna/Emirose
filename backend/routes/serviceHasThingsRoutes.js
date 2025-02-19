7ee442bd6b4e726404a01c23281574ee7af52047
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-19 11:36:45 +0530

    Update serviceHasThingsRoutes.js

--- backend/routes/serviceHasThingsRoutes.js
+++ backend/routes/serviceHasThingsRoutes.js
@@ -7,6 +7,7 @@ router.post("/", serviceHasThingsController.createServiceHasThing);
 router.get("/:serviceId", serviceHasThingsController.getAllServiceHasThings);
 router.get("/thing/:id", serviceHasThingsController.getServiceHasThingById);
 router.put("/thing/:id", serviceHasThingsController.updateServiceHasThing);
+router.put("/:serviceId", serviceHasThingsController.updateBulkServiceHasThings);
 router.delete("/thing/:id", serviceHasThingsController.deleteServiceHasThing);
 
 module.exports = router;
