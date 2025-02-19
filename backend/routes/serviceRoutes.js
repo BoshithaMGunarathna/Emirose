5f600a2b9c03488fea9b8271d4c2b15a66c969db
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-19 11:53:23 +0530

    Update serviceRoutes.js

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/", uploadServiceMiddleware.single("Image"), serviceController.createService);
 router.get("/", serviceController.getAllServices);
 router.get("/:id", serviceController.getServiceById);
+router.get('/withSub/:serviceId', serviceController.getServiceWithSubServices);
 router.put("/:id", uploadServiceMiddleware.single("Image"), serviceController.updateService);
 router.delete("/:id", serviceController.deleteService);
 
