0fb8f5431df247c0d30ec925b1474d82f0a8ad9e
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-19 10:22:49 +0530

    Create productController.js

---
+++ backend/controllers/productController.js
@@ -0,0 +1,67 @@
+const productService = require('../services/productService');
+
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const baseURL = `${req.protocol}://${req.get("host")}/uploads/`;
+        const products = await productService.getAllProducts(baseURL); // Pass baseURL to service
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getProductById = async (req, res) => {
+    try {
+        const baseURL = `${req.protocol}://${req.get("host")}/product-images/`;
+        const product = await productService.getProductById(req.params.id, baseURL);
+        
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        
+        res.json(product);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+exports.createProduct = async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: "Image is required" });
+        }
+
+        const imagePath = req.file.filename;
+        const productData = {
+            Name: req.body.Name,
+            Price: req.body.Price,
+            Discount: req.body.Discount,
+            image: imagePath,
+        };
+
+        const newProduct = await productService.createProduct(productData);
+        res.status(201).json(newProduct);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.updateProduct = async (req, res) => {
+    try {
+        const updatedProduct = await productService.updateProduct(req.params.id, req.body);
+        res.json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const message = await productService.deleteProduct(req.params.id);
+        res.json(message);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
