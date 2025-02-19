cbe2a2ef97afb6f567db9e1e6b6bc3655b41aa20
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2025-02-19 10:38:12 +0530

    Create serviceHasThingsRoutes.js

---
+++ backend/routes/serviceHasThingsRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const serviceHasThingsController = require('../controllers/serviceHasThingsController');
+
+const router = express.Router();
+
+router.post("/", serviceHasThingsController.createServiceHasThing);
+router.get("/:serviceId", serviceHasThingsController.getAllServiceHasThings);
+router.get("/thing/:id", serviceHasThingsController.getServiceHasThingById);
+router.put("/thing/:id", serviceHasThingsController.updateServiceHasThing);
+router.delete("/thing/:id", serviceHasThingsController.deleteServiceHasThing);
+
+module.exports = router;
